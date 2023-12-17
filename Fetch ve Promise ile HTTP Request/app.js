5a21210c443e8d5390be23a41ce51756953bda74
theplayernumberzero <bxkuygulamalar.swe@gmail.com>
2023-12-17 10:52:17 +0300

    fetch HTTP request

--- Fetch ve Promise ile HTTP Request/app.js
+++ Fetch ve Promise ile HTTP Request/app.js
@@ -28,6 +28,32 @@ class Request{
         });
         
     }
+
+    put(url, data){
+        return new Promise((resolve, reject) => {
+
+            fetch(url, {
+                method: 'POST',
+                body: JSON.stringify(data),
+                headers: {
+                    'Content-type': 'application/json; charset=UTF-8',
+                },
+            })
+            .then(response => response.json)
+            .then(json => resolve(json))
+            .catch(err => reject(err))
+
+        });
+    }
+    delete(url){
+        return new Promise((resolve, reject) => {
+            fetch(url, {
+            method: 'DELETE',
+            })
+            .then(response => resolve("Veri silme işlemi başarılı.."))
+            .catch(err => reject(err));
+        });
+    }
 }
 
 const request = new Request();
@@ -38,8 +64,16 @@ request.get("https://jsonplaceholder.typicode.com/albums")
     albums = data;
     console.log(albums);    //asenkron çalıştığı için albums değer aldığı anda yazdırılmaya çalışılmalı
 })
-.catch(err => console.log(err));
+.catch(err => console.error(err));
 
-request.post("https://jsonplaceholder.typicode.com/albums",{userId:1, title:"Harry Potter", })
+request.post("https://jsonplaceholder.typicode.com/albums",{userId:1, title:"Harry Potter"})
 .then(newAlbum => console.log(newAlbum))
-.catch(err => console.log(err));
+.catch(err => console.error(err));
+
+request.put("https://jsonplaceholder.typicode.com/albums/1", {userId:10, title:"James Bond"})
+.then(editedAlbum => console.log(editedAlbum))
+.catch(err => console.error(err));
+
+request.delete("https://jsonplaceholder.typicode.com/albums/2")
+.then(message => console.log(message))
+.catch(err => console.error(err));
